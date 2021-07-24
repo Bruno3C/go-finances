1f08bafe1fd52173bdb0cfe4e9817a92f43ab9fb
Bruno3C <brunodossantos.job@gmail.com>
2021-07-24 17:06:20 -0300

    Utilizando proporções em medidas

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { Container, Header, UserWrapper, UserInfo, Photo, User, UserGreeting, UserName } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Teste</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/43102163?v=4.png'}}/>
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Rodrigo</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
