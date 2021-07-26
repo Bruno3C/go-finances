e47e69f7737c81b98f4f9e89a287f999bd414bcb
Bruno3C <brunodossantos.job@gmail.com>
2021-07-26 16:29:58 -0300

    Estilização da interface para selecionar categoria

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <CategorySelect />
     </ThemeProvider>
   );  
 }
---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import { Button } from '../../components/Forms/Button';
+import { categories } from '../../utils/categories';
+
+import {
+  Container,
+  Header,
+  Title,
+  Category,
+  Icon,
+  Name,
+  Separator,
+  Footer
+} from './styles';
+
+interface Category {
+  key: string;
+  name: string;
+}
+
+interface Props {
+  category: string;
+  setCategory: (category: Category) => void;
+  closeSelectCategory: () => void;
+}
+
+export function CategorySelect({
+  category,
+  setCategory,
+  closeSelectCategory
+}: Props) {
+  return (
+    <Container>
+      <Header>
+        <Title>Categoria</Title>
+      </Header>
+
+      <FlatList 
+        data={categories}
+        style={{flex: 1, width: '100%'}}
+        keyExtractor={item => item.key}
+        renderItem={({item}) => (
+          <>
+            <Category>
+              <Icon name={item.icon} />
+              <Name>{item.name}</Name>
+            </Category>
+            <Separator/>
+          </>
+          
+        )}
+      />
+
+      <Footer>
+        <Button title="Selecionar" />
+      </Footer>
+    </Container>
+  );
+}
