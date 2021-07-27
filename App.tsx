121ab88ddf1b7c6d32e6a1003bee906e4e4e6ba3
Bruno3C <brunodossantos.job@gmail.com>
2021-07-27 10:44:56 -0300

    Utilizando a tab navigation

--- App.tsx
+++ App.tsx
@@ -11,7 +11,8 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );  
 }
