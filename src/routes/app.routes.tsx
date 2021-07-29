6e24b7bba0f879843e11bdd9173d1b480a40bd12
Bruno3C <brunodossantos.job@gmail.com>
2021-07-28 21:23:30 -0300

    Criando a interface de grafico

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -8,6 +8,7 @@ import {
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { Resume } from '../screens/Resume';
 
 const {
   Navigator,
@@ -57,7 +58,7 @@ export function AppRoutes() {
       />
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: (({ size, color}) => (
             <MaterialIcons
---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { HistoryCard } from '../../components/HistoryCard';
+
+import {
+  Container,
+  Header,
+  Title
+} from './styles';
+
+export function Resume() {
+  return (
+    <Container>
+      <Header>
+        <Title>Resumo por categoria</Title>
+      </Header>
+
+      <HistoryCard 
+        color="red"
+        title="Compras"
+        amount="R$ 150,50"
+      />
+    </Container>
+  );
+}
